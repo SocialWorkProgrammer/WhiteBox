0fbd8d896356ab2607c1406b973e713ae552672a
InbumSong <sib0817@naver.com>
2024-09-06 16:36:09 +0900

    :tada: init project

---
+++ whitebox/src/main/java/com/ssafy/whitebox/WhiteboxApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.whitebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteboxApplication.class, args);
+	}
+
+}
