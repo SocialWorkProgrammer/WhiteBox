ec90e764e18f58a582c7badab2abd38792b70ee4
InbumSong <sib0817@naver.com>
2024-09-13 15:57:34 +0900

    add :  relatedSites api

---
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/info/entity/RelatedSites.java
@@ -0,0 +1,42 @@
+package com.ssafy.whitebox.info.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true)
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RelatedSites {
+
+    @Id
+    @Column(name = "rs_idx")
+    private Long index;
+
+    @Column(name="rs_category")
+    private String category;
+
+    @Column(name= "rs_name")
+    private String name;
+
+    @Column(name= "rs_url")
+    private String url;
+
+    public RelatedSites(String category, String name, String url){
+        this(null, category, name, url);
+    }
+
+    public RelatedSites(Long index, String category, String name, String url) {
+        this.index = index;
+        this.category = category;
+        this.name = name;
+        this.url = url;
+    }
+}
