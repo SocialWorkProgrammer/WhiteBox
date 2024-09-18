733f02041d4c2479889aff3561dcb9ae8ae6e318
sib0817 <sib0817@naver.com>
2024-09-19 00:31:43 +0900

    update: add data

---
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/info/dto/LawParam.java
@@ -0,0 +1,4 @@
+package com.ssafy.whitebox.info.dto;
+
+public class LawParam {
+}
--- backend/whitebox/src/main/java/com/ssafy/whitebox/info/entity/RelatedSites.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/info/entity/RelatedSites.java
@@ -1,9 +1,6 @@
 package com.ssafy.whitebox.info.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +14,7 @@ import lombok.experimental.Accessors;
 public class RelatedSites {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rs_idx")
     private Long index;
 
