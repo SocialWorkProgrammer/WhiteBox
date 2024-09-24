a71df627b1a84ebe721793ca72e4a00c599d4165
InbumSong <sib0817@naver.com>
2024-09-24 17:19:01 +0900

    hotfix cors config

--- backend/whitebox/src/main/java/com/ssafy/whitebox/common/config/CorsMvcConfig.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/common/config/CorsMvcConfig.java
@@ -8,11 +8,11 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-
         corsRegistry.addMapping("/**")
-                 .allowedOrigins("http://localhost:3000");
-                    //.allowedOrigins("*")
-                    //.allowedMethods("GET", "POST", "PUT", "DELETE")
-                    //.allowedHeaders("*");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 인증 정보 허용
+
     }
 }
