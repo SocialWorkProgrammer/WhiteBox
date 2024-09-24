ee3d81df83fe717f9fdb0257ee2dd3c5f66769dd
차재훈 <wogns264@gmail.com>
2024-09-24 09:39:17 +0900

    Feat: ㄴㅁ아ã썸네일 추출 기능 완료

--- backend/whitebox/src/main/java/com/ssafy/whitebox/ai/service/AIResultService.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/ai/service/AIResultService.java
@@ -3,6 +3,7 @@ package com.ssafy.whitebox.ai.service;
 import com.ssafy.whitebox.ai.entity.AIResult;
 import com.ssafy.whitebox.ai.repository.AIResultRepository;
 import com.ssafy.whitebox.common.service.FileStorageService;
+import com.ssafy.whitebox.common.service.FFmpegService;
 import com.ssafy.whitebox.user.entity.User;
 import com.ssafy.whitebox.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +14,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.ssafy.whitebox.ai.dto.AIAnalysisResult;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.client.MultipartBodyBuilder;
 import reactor.core.publisher.Mono;
-import org.springframework.core.io.InputStreamResource;
-
-
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -31,17 +28,22 @@ public class AIResultService {
     private final AIResultRepository aiResultRepository;
     private final UserRepository userRepository;
     private final FileStorageService fileStorageService;
+    private final FFmpegService ffmpegService; // FFmpegService 인스턴스 주입
     private final WebClient webClient; // WebClient 인스턴스 주입
 
     // FastAPI 서버 URL
     @Value("${fastapi.server.url}")
     private String fastApiUrl;
 
-    public AIResult createAIResult(Long userId, MultipartFile videoFile) throws IOException {
+    public AIResult createAIResult(Long userId, MultipartFile videoFile) throws IOException, InterruptedException {
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
 
         // 비디오 파일 저장
         String videoUrl = fileStorageService.saveFile(videoFile, "video");
+
+        // 썸네일 추출 및 저장
+        String thumbnails = ffmpegService.extractThumbnails(videoFile);
+        String[] thumbnailPaths = thumbnails.split(", ");
 
         // FastAPI로 비디오 파일 전송 및 AI 분석 결과 받기
         AIAnalysisResult analysisResult = analyzeVideo(videoFile);
@@ -55,14 +57,16 @@ public class AIResultService {
                 .aiUserFault(analysisResult.getAiUserFault())
                 .aiOtherFault(analysisResult.getAiOtherFault())
                 .videoUrl(videoUrl)
-                .thumbnail1("thumbnail1.jpg")
-                .thumbnail2("thumbnail2.jpg")
-                .thumbnail3("thumbnail3.jpg")
+                .thumbnail1(thumbnailPaths[0]) // 실제 저장된 썸네일 경로
+                .thumbnail2(thumbnailPaths[1])
+                .thumbnail3(thumbnailPaths[2])
+                .thumbnail4(thumbnailPaths[3])
                 .isUploaded(false)
                 .build();
 
         return aiResultRepository.save(aiResult);
     }
+
     private AIAnalysisResult analyzeVideo(MultipartFile videoFile) throws IOException {
         WebClient client = WebClient.builder()
                 .baseUrl(fastApiUrl)
@@ -71,7 +75,7 @@ public class AIResultService {
         MultipartBodyBuilder builder = new MultipartBodyBuilder();
         builder.part("file", videoFile.getResource())
                 .filename(videoFile.getOriginalFilename());
-        byte[] videoBytes = videoFile.getBytes();
+
         return client.post()
                 .uri("/analyze-video/")
                 .contentType(MediaType.MULTIPART_FORM_DATA)
@@ -83,6 +87,7 @@ public class AIResultService {
                 .bodyToMono(AIAnalysisResult.class)
                 .block();
     }
+
     public List<AIResult> getAllAIResults() {
         return aiResultRepository.findAll();
     }
