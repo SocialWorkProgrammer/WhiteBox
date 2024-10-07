515eac0867a114644935e4e25a6ab088f82b8e84
송인범 <sib0817@naver.com>
2024-10-07 22:59:23 +0900

    Update CorsMvcConfig.java

--- backend/whitebox/src/main/java/com/ssafy/whitebox/common/config/CorsMvcConfig.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/common/config/CorsMvcConfig.java
@@ -1,4 +1,5 @@
 package com.ssafy.whitebox.common.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +10,10 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://j11a104.p.ssafy.io") // 로컬 및 배포된 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true); // 인증 정보 허용
-
     }
 }
