2d8e05696f08f1fcc67f8861346c7ce3a7bf7ee8
차재훈 <wogns264@gmail.com>
2024-10-08 17:25:19 +0900

    Feat: 이미지 url 변경

--- backend/whitebox/src/main/java/com/ssafy/whitebox/community/dto/CommunityImageParam.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/community/dto/CommunityImageParam.java
@@ -13,7 +13,7 @@ public class CommunityImageParam{
 
     public CommunityImageParam(Long id, String imageUrl, LocalDateTime createdAt){
         this.id = id;
-        this.imageUrl = imageUrl;
+        this.imageUrl = "/images/" + imageUrl;
         this.createdAt = createdAt;
     }
 
