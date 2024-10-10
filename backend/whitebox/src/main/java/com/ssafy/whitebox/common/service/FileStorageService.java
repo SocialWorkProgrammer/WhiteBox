e78c2cabf1fa1d2474388159b306c52dc39e39d1
InbumSong <sib0817@naver.com>
2024-10-10 19:20:24 +0900

    ffpmeg

--- backend/whitebox/src/main/java/com/ssafy/whitebox/common/service/FileStorageService.java
+++ backend/whitebox/src/main/java/com/ssafy/whitebox/common/service/FileStorageService.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.UUID;
 
 @Service
@@ -60,5 +61,48 @@ public class FileStorageService {
             return fileName.substring(fileName.lastIndexOf("."));
         }
         return "";
+    }
+
+
+    public String compressAndSaveFile(MultipartFile file, String type) throws IOException, InterruptedException {
+
+        Path tempDir = Files.createTempDirectory("");
+        File originalFile = new File(tempDir.toFile(), file.getOriginalFilename());
+        file.transferTo(originalFile);
+
+        // 압축된 파일을 저장할 경로 설정
+        String compressedFileName = "compressed_" + file.getOriginalFilename();
+        File compressedFile = new File(tempDir.toFile(), compressedFileName);
+
+        // 비디오 파일 압축 (1/3 크기)
+        compressVideo(originalFile, compressedFile);
+
+        // 압축된 파일을 S3에 업로드
+        String compressedVideoUrl = saveFile(Files.readAllBytes(compressedFile.toPath()), compressedFileName, type);
+
+        // 임시 파일 삭제
+        originalFile.delete();
+        compressedFile.delete();
+
+        return compressedVideoUrl;
+    }
+
+    private void compressVideo(File inputFile, File outputFile) throws IOException, InterruptedException {
+        // FFmpeg 명령어 구성 (비트레이트 줄여서 압축)
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "ffmpeg", "-i", inputFile.getAbsolutePath(),
+                "-vcodec", "h264", "-acodec", "aac", "-strict", "-2",
+                "-b:v", "1000k", "-b:a", "128k", // 비트레이트 조정
+                outputFile.getAbsolutePath()
+        );
+
+        // FFmpeg 실행
+        Process process = processBuilder.start();
+
+        // FFmpeg 완료 대기
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new IOException("FFmpeg failed to compress the video");
+        }
     }
 }
