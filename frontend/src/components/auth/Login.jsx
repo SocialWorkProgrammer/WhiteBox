e202affe9d086158ef8fe874880656b9749a4e83
JHS <ghtjd1551@naver.com>
2024-10-10 15:13:29 +0900

    design: ui ux

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 import useAuthStore from "../../store/useAuthStore";
 import { Helmet } from 'react-helmet';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Login() {
+    const [ isLoading, setIsLoading ] = useState(false);
     const navigate = useNavigate();
     const { login, getUser, getAccessToken } = useAuthStore();
 
@@ -22,18 +24,21 @@ function Login() {
 
     const handleOnSubmit = async (event) => {
         event.preventDefault();
+        setIsLoading(true)
         const response = await login({...user});
         if (response.isSuccess) {
             const nickname = response.nickname;
             navigate(`/auth/profile/${nickname}`);
+            setIsLoading(false)
         } else {
+            setIsLoading(false)
             alert("등록되지 않은 사용자이거나, 비밀번호가 일치하지 않습니다.");
             return;
         }
     };
 
     return (
-        <div>
+        <div className={`login-container ${isLoading ? 'blur' : ''}`}>
             <Helmet>
                 <title>White Box | 로그인</title>
             </Helmet>
@@ -43,16 +48,21 @@ function Login() {
                 <form className="login-form-container" onSubmit={handleOnSubmit}>
                     <div className="login-form-group">
                         <label htmlFor="id">이메일</label>
-                        <input type="email" id="id" name="id" value={user.id} onChange={handleOnChange} required/>
+                        <input type="email" id="id" name="id" value={user.id} onChange={handleOnChange} required disabled={isLoading}/>
                     </div>
                     <div className="login-form-group">
                         <label htmlFor="password">비밀번호</label>
-                        <input type="password" id="password" name="password" value={user.password} onChange={handleOnChange} required/>
+                        <input type="password" id="password" name="password" value={user.password} onChange={handleOnChange} required disabled={isLoading}/>
                     </div>
 
-                    <button className="login-button" type="submit">로그인</button>
+                    <button className="login-button" type="submit" disabled={isLoading}>로그인</button>
                 </form>
             </div>
+            {isLoading && (
+                <div className="login-clip-loader">
+                    <ClipLoader />
+                </div>
+            )}
         </div>
     );
 };
