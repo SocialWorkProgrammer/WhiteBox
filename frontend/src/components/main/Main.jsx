9eb39068b07748129a0d4111b73d753d827c0855
minho2633 <minho2633@naver.com>
2024-10-11 10:36:37 +0900

    feat: 메인화면 메뉴얼 gif 추가

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -7,6 +7,7 @@ import useAIStore from '../../store/useAIStore.jsx'
 import { Helmet } from 'react-helmet';
 import "../../styles/main/main.css";
 import mainGIF from '../../public/img/sample.gif'
+import mainManualGIF from '../../public/img/main-manual.gif'
 
 function Main() {
     const navigate = useNavigate();
@@ -105,9 +106,14 @@ function Main() {
                     <div className="col-span-2">
                     </div>
                     {/* gif 파일 */}
-                    <div className="col-span-4 hidden md:flex items-center justify-center">
+                    {isLogin ? 
+                    (<div className="col-span-4 hidden md:flex items-center justify-center">
                         <img src={mainGIF} alt="GIF" className="shadow-xl max-w-full max-h-full bg-red-300" />
-                    </div>
+                    </div>)
+                    :(<div className="col-span-4 hidden md:flex items-center justify-center">
+                        <img src={mainManualGIF} className='shadow-xl max-w-full max-h-full bg-red-300' alt="" />
+                    </div>)
+                    }
                     
                     {/* 비디오 드래그 앤 드롭 */}
                     <div className='col-span-8 flex items-center justify-center md:col-span-4'>
